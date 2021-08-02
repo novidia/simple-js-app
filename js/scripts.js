b0163914d6e2b56b0f4189d7db30447ab46445b3
Novidia <87478105+novidia@users.noreply.github.com>
2021-08-02 14:34:29 +0200

    Update Script: Adding Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 let pokemon = [
 
-{ name: 'Lunatone',
-  speed: 70,
-  types: ['psychic', 'rock']},
+{name: 'Lunatone',
+ speed: 70,
+ types: ['psychic', 'rock']},
 
-{ name: 'Celebi',
-  speed: 100,
-  types: ['psychic', 'grass']},
+{name: 'Celebi',
+ speed: 100,
+ types: ['psychic', 'grass']},
   
-{ name: 'Sigilyph',
-  speed: 97,
-  types: ['psychic', 'flying']},
+{name: 'Sigilyph',
+ speed: 97,
+ types: ['psychic', 'flying']},
 
-{ name: 'Meloetta',
-  speed: 90,
-  types: ['psychic', 'normal']}, 
+{name: 'Meloetta',
+ speed: 90,
+ types: ['psychic', 'normal']}, 
   
 {name: 'Jirachi',
-  speed: 100,
-  types: ['psychic', 'steel']}
+ speed: 100,
+ types: ['psychic', 'steel']}
 
 ]
+
+console.log(pokemon)
